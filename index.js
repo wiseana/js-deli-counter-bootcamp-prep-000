f5350fc18eae0a1d2ef94c9f60647fb19d4eb7dd
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-10 15:26:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine,name) {katzDeliLine.push (name); return `Welco
 
 function nowServing(array) {if (array.length>0) {var output=[]; output=array.shift(1); return `Currently serving ${output}.`} else {return "There is nobody waiting to be served!";}}
 
-function theLine(katzDeliLine) {
+function currentLine(katzDeliLine) {
   let count=0; var output=[]; 
   while (count<katzDeliLine.length) {output.push (`${count+1}. ${katzDeliLine[count]}`); ++count}
   return `The line is currently: ${output}`;}
